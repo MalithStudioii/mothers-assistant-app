347a83a379fdffb14dff836d20836c3562c113e0
MalithStudioii <silvadilan9@gmail.com>
2026-07-17 14:41:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,31 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11301980
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+const CACHE_NAME = 'mothers-app-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './names.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
